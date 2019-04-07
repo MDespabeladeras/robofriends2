41f64ebaa39823e8910701e3e2ee6684e805d72c
Marilou Despabeladeras <mdespabeladeras@gmail.com>
2019-04-07 17:40:17 -0600

    my robofriends2 first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor() {
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+	}
+
+	componentDidMount() {
+		this.setState({ robots: robots});
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value})
+		
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robots => {
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		return (
+		<div className='tc'>
+			<h1 className='f1'>Robofriends2</h1>
+			<SearchBox searchChange={this.onSearchChange}/>
+			<scroll>
+			<CardList robots={filteredRobots}/>
+			</scroll>
+		</div>
+	);	
+	}
 }
+
 
 export default App;
---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Len Graham',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 2,
+	name: 'Ben Gray',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 3,
+	name: 'Ana Pat',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 4,
+	name: 'Jane Doer',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 5,
+	name: 'Anita Color',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 6,
+	name: 'Jen Arn',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 7,
+	name: 'Lila Thor',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 8,
+	name: 'Gre Gregg',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 9,
+	name: 'Steph Young',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 10,
+	name: 'Brad Pitty',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 11,
+	name: 'Johnny Down',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+{
+	id: 12,
+	name: 'Erin Brown',
+	username: 'Bret',
+	email: 'Sincere@april.biz'
+},
+]
